7752f238cf24936604d57168764624f03faf3839
Jesse Greene <jfg3rd@gmail.com>
2026-06-23 14:11:11 +0200

    Initial Commit

---
+++ RoyalTranslator/RoyalTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoyalTranslatorApp.swift
+//  RoyalTranslator
+//
+//  Created by Jesse Greene on 23.06.26.
+//
+
+import SwiftUI
+
+@main
+struct RoyalTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
